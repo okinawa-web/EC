93fcea87b191cd6e3437145593c0d77d5bbdf2ac
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-01-16 13:52:56 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 
+
+
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
