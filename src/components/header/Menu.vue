ed75be830a9998f4d8d61c693986c3c6ccc696c9
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-01-18 13:11:54 +0900

    ハンバーガーMENU作成

---
+++ src/components/header/Menu.vue
@@ -0,0 +1,130 @@
+<template>
+  <header class="header">
+    <!-- ハンバーガーメニュー部分 -->
+    <div class="nav">
+      <!-- 表示・非表示を切り替えるチェックボックス -->
+      <input id="drawer_input" class="drawer_hidden" type="checkbox" />
+      <!-- ハンバーガーアイコン -->
+      <label for="drawer_input" class="drawer_open"><span></span></label>
+      <!-- <label for="drawer_text" class="drawer_open">MENU</label> -->
+
+      <!-- MENU -->
+      <nav class="nav_content">
+        <ul class="nav_list">
+          <li class="nav_item"><a href="">TOP</a></li>
+          <li class="nav_item"><a href="">ご予約</a></li>
+          <li class="nav_item"><a href="">●●について</a></li>
+          <li class="nav_item"><a href="">お部屋</a></li>
+          <li class="nav_item"><a href="">-101</a></li>
+          <li class="nav_item"><a href="">-102</a></li>
+          <li class="nav_item"><a href="">-103</a></li>
+          <li class="nav_item"><a href="">-客室設備・アメニティ</a></li>
+          <li class="nav_item"><a href="">アクティビティ</a></li>
+          <li class="nav_item"><a href="">アクセス</a></li>
+          <li class="nav_item"><a href="">よくあるご質問</a></li>
+          <li class="nav_item"><a href="">新着情報</a></li>
+          <li class="nav_item"><a href="">会社概要</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  background: #fff;
+}
+
+.logo {
+  font-size: 24px;
+}
+
+/* ここから下がハンバーガーメニューに関するCSS */
+/* チェックボックスを非表示にする */
+.drawer_hidden {
+  display: none;
+}
+
+/* ハンバーガーアイコンの設置スペース */
+.drawer_open {
+  display: flex;
+  height: 60px;
+  width: 60px;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 100; /*重なり順を一番上にする */
+  cursor: pointer;
+}
+
+/* ハンバーガーメニューのアイコン */
+.drawer_open span,
+.drawer_open span:before,
+.drawer_open span:after {
+  content: "";
+  display: block;
+  height: 3px;
+  width: 25px;
+  border-radius: 3px;
+  background: #333;
+  transition: 0.5s;
+  position: absolute;
+}
+
+/* 三本線の一番上の棒の位置調整 */
+.drawer_open span:before {
+  bottom: 8px;
+}
+
+/* 三本線の一番下の棒の位置調整 */
+.drawer_open span:after {
+  top: 8px;
+}
+
+/* クリックされたら真ん中の線を透明にする */
+#drawer_input:checked ~ .drawer_open span {
+  background: rgba(255, 255, 255, 0);
+}
+
+/* クリックされたらアイコンが×になように上下の線を回転 */
+#drawer_input:checked ~ .drawer_open span::before {
+  bottom: 0;
+  transform: rotate(45deg);
+}
+
+#drawer_input:checked ~ .drawer_open span::after {
+  top: 0;
+  transform: rotate(-45deg);
+}
+
+/* クリックされたらMENUがCLOSEになる */
+/* #drawer_text:checked ~ .drawer_open::after{
+        content: 'CLOSE';
+    } */
+
+/* メニューのデザイン*/
+.nav_content {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 100%; /* メニューを画面の外に飛ばす */
+  z-index: 99;
+  background: #fff;
+  transition: 1s;
+}
+
+/* メニューリスト点を消す */
+.nav_list {
+  list-style: none;
+}
+
+/* アイコンがクリックされたらメニューを表示 */
+#drawer_input:checked ~ .nav_content {
+  left: 0; /* メニューを画面に入れる */
+}
+</style>
