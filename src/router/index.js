07a42f9c13ef51795e6a4c37833d17f33389af43
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-01-19 14:50:35 +0900

    ヘッダー リンク作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutRikyu from "../views/AboutRikyu.vue";
+import Rooms from "../views/Rooms.vue";
+import Activity from "../views/Activity.vue";
+import Acsess from "../views/Acsess.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/test',
-      name: 'test',
+      path: "/test",
+      name: "test",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/test.vue')
-    }
-  ]
-})
+      component: () => import("../views/test.vue"),
+    },
+    {
+      path: "/aboutRikyu",
+      name: "AboutRikyu",
+      component: AboutRikyu,
+    },
+    {
+      path: "/rooms",
+      name: "Rooms",
+      component: Rooms,
+    },
+    {
+      path: "/activity",
+      name: "Activity",
+      component: Activity,
+    },
+    {
+      path: "/acsess",
+      name: "Acsess",
+      component: Acsess,
+    },
+  ],
+});
 
-export default router
+export default router;
