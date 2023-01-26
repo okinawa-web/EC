dee467a88364727875ac8c4640a21a7b27d24e6e
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-01-26 12:11:51 +0900

    aboutrikyu_privetevillaとBasicButton作成編集

--- src/components/button/BasicButton.vue
+++ src/components/button/BasicButton.vue
@@ -1,17 +1,72 @@
 <template>
-  <a href="#">SAMPLE</a>
+  <!-- <a href="#">SAMPLE</a> -->
+  <div class="basic">
+    <router-link :to="`/${props.link}`" class="basic_btn">
+      ➡︎ {{ props.linkName }}
+    </router-link>
+  </div>
 </template>
 
+<script setup>
+const props = defineProps({
+  link: String,
+  linkName: String,
+});
+</script>
+
 <style>
-a {
+.basic {
+  position: relative;
+  /* margin-left: auto;
+  margin-right: auto; */
+  width: 150px;
+  height: 48px;
+  line-height: 48px;
+  display: flex;
+  text-align: center;
+  background: #fff;
+  font-size: 20px;
+}
+
+.basic_btn {
+  width: 100%;
+  height: 100%;
+  color: darkgray;
+  z-index: 10;
+}
+
+.basic_btn:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 0;
   display: block;
-  color: white;
+  background: darkgray;
+  z-index: -1;
+  transition: 0.5s;
+}
+
+.basic_btn:hover {
+  color: #fff;
+}
+
+.basic_btn:hover:before {
+  width: 100%;
+}
+
+/* .basic_btn {
+  display: block;
   font-size: 16px;
-  background: #7d8384;
   padding: 18px 30px;
   transition: width 500ms ease;
-}
-a:hover {
-  background: #7d8384;
-}
+  width: 6%;
+  height: 15px;
+  color: darkgray;
+  background-color: aquamarine;
+} */
+/* .basic_btn:hover {
+  background: darkgray;
+  color: white;
+} */
 </style>
