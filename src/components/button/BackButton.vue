66f91598f8e0fcfa4cf0e27dfc9e9893021098d1
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-02-01 17:11:12 +0900

    reserve BackButton作成

---
+++ src/components/button/BackButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="button_flame">
+    <router-link to="/reserve/reserve" class="back_button"
+      >前の画面に戻る</router-link
+    >
+  </div>
+</template>
+
+<style>
+.button_flame {
+  padding: 0.5em 1em;
+  margin: 2em 0;
+  color: darkgray;
+  background: darkgray;
+  border-bottom: solid 6px orange;
+  border-radius: 5px;
+  width: 10%;
+}
+.back_button {
+  /* background-color: darkgray;
+    width: 30%;
+    height: 20%; */
+  margin: 0;
+  padding: 0;
+}
+.back_button::before {
+  content: "> ";
+}
+</style>
