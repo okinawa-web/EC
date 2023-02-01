c1376e28f3fc9bf51f56a4bb3fd1f2f1dfd3e05f
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-02-01 17:28:32 +0900

    reserve 確認/キャンセルbutton作成

---
+++ src/components/button/OkCancelButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="button_flame">
+    <router-link :to="`/${props.linkName}`" class="back_button"
+      >確認/キャンセル{{ props.link }}</router-link
+    >
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  link: String,
+  linkName: String,
+});
+</script>
+
+<style>
+.button_flame {
+  padding: 0.5em 1em;
+  margin: 2em 0;
+  color: darkgray;
+  background: orange;
+  border-bottom: solid 6px darkgray;
+  border-radius: 5px;
+  width: 10%;
+}
+.back_button {
+  margin: 0;
+  padding: 0;
+}
+.back_button::before {
+  content: "> ";
+}
+</style>
