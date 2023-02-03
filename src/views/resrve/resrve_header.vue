afbf1bc9ac466ae7f18a8b80be416388b5b10d85
Ikuri Kijima <ikuri.kijima@rakus-partners.co.jp>
2023-02-03 11:46:22 +0900

    resrve_header 作成

---
+++ src/views/resrve/resrve_header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header_all">
+    <div class="header_bar">
+      <router-link to="/" class="header_link"> 予約の確認 </router-link>
+      <router-link to="/" class="header_link"> 予約の変更 </router-link>
+      <router-link to="/" class="header_link"> 予約のキャンセル </router-link>
+    </div>
+    <div class="login">
+      <router-link to="/" class="header_link"> ログイン </router-link>
+    </div>
+  </div>
+</template>
+
+<style setup>
+.header_all {
+  display: flex;
+}
+.header_bar {
+  background-color: darkgray;
+  width: 900px;
+  height: 35px;
+  text-align: right;
+}
+.header_link {
+  color: white;
+  margin: auto 20px;
+}
+.login {
+  background-color: rgb(14, 109, 130);
+  width: 120px;
+  text-align: center;
+}
+</style>
