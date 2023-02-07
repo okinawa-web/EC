e0cdda5c9ecc21fc94a9865d4a8da86690158f95
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-02-07 10:30:38 +0900

    top_Fresh

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,75 +1,123 @@
 <template>
-  <div class="room">
-    <Pagetop :pagetitle="page" :image="pic" :subTitle="sub" />
-    <div class="coments">
-      <p>
-        広々としたオープンスタイルのリビングから見えるプライベートテラスやプールをはじめ、
-      </p>
-      <p>リゾート感あふれる空間で、のんびりとお過ごしいただけます。</p>
-      <Reserve class="reserve_btn" />
+  <div class="top_fresh">
+    <div class="fresh">
+      <p>波の音、清々しい</p>
+      <p>心休めは贅沢な時間とともに</p>
     </div>
-
-    <div class="items">
-      <div class="item">
-        <div class="terrace">
-          <p>Terrace & Pool</p>
-          <img src="@/assets/terasu.jpg" alt="" class="image" />
+    <div class="fresh_info">
+      <!-- <div class="news-banner">
+        <div class="news-banner__content">
+          <p>HAVE A GREAT HOLIDAY</p>
         </div>
-        <div class="bath">
-          <p>Bath</p>
-          <img src="@/assets/toilet.jpg" alt="" class="image" />
-        </div>
-        <div class="kitchen">
-          <p>Kitchen</p>
-          <img src="@/assets/room1.jpg" alt="" class="image" />
+      </div> -->
+      <div class="fresh_img">
+        <p class="img1">
+          <img src="@/assets/umi.jpg" alt="topimg" />
+        </p>
+        <!-- <div class="flex"> -->
+        <p class="img2">
+          <img src="@/assets/yuuhi.jpg" alt="topimg" width="60%" />
+        </p>
+      </div>
+    </div>
+    <div class="text_fresh">
+      <!-- <p>
+        都会の騒音から離れ、ゆっくりと心から深呼吸。<br />
+        中心地から程よく離れた海を望む高台に佇む<br />
+        HAMAJIMA<br />
+        お部屋から望む海を眺めながら、自由に、赴くままに。<br />
+        大切な人と大切な時を刻む、極上のひとときをお過ごしください。
+      </p> -->
+      <div class="news-banner">
+        <div class="news-banner__content">
+          <p>HAVE A GREAT HOLIDAY</p>
         </div>
       </div>
     </div>
-    <Info />
+    <!-- <BasicButton :link="site" :linkName="siteName" /> -->
   </div>
+  <!-- </div> -->
 </template>
-
+<!-- 
 <script setup>
-import Pagetop from "@/components/PageTop.vue";
-import Reserve from "@/components/header/Reserve.vue";
-import Info from "@/components/room/Information.vue";
+import BasicButton from "@/components/button/BasicButton.vue";
 import { ref } from "vue";
 
-//Pagetop
-const page = ref("Room 101");
-const pic = ref("room1.jpg");
-const sub = ref("101号室");
-</script>
+//ボタン
+const site = ref("activity");
+const siteName = ref("Activity");
+</script> -->
 
 <style>
-.room {
+.top_fresh {
   color: darkgray;
 }
-.items {
-  color: darkgray;
-  font-size: 200%;
-  background-color: rgb(226, 238, 248);
-  width: 90%;
-  margin: auto;
-}
-.item {
-  color: darkgray;
-  font-size: 200%;
-  width: 70%;
-  margin: auto;
-}
-.coments {
+.fresh {
+  font-size: 25px;
+  line-height: 100%;
   text-align: center;
 }
-.reserve_btn {
-  /* text-align: center; */
-  margin-left: 40%;
-  margin-bottom: 10%;
+.fresh_info {
+  /* display: flex; */
 }
-.image {
-  width: 50%;
+.text_fresh {
+  /* position: relative; */
 }
-.bath {
-  text-align: right;
+.fresh_img {
+  /* position: absolute; */
+  /* width: 30%; */
+  /* float: left; */
+  position: relative;
+}
+.img1 {
+  /* position: relative;
+  width: 30%; */
+  position: absolute;
+  /* z-index: 1; */
+}
+.img2 {
+  position: absolute;
+  /* right: 5%; */
+  /* margin: 10% 0% 10% 90%; */
+  /* margin-left: 50%; */
+  /* margin-top: 20%; */
+  /* width: 60%; */
+  /* bottom: 0; */
+  right: 0;
+  margin-top: 10%;
+}
+
+/* バナー本体 */
+.news-banner {
+  position: relative;
+  display: block;
+  height: 250px;
+  width: 100%;
+  /* background-color: orange; */
+  overflow: hidden;
+}
+/* バナー内コンテンツ */
+.news-banner__content {
+  display: inline-block;
+  color: darkgray;
+  line-height: 0px;
+  padding-left: 100%;
+  white-space: nowrap;
+  animation: animate-banner 50s linear infinite;
+  font-size: 150px;
+  font-weight: 900;
+}
+/* バナー内の要素をすべて選択 */
+.news-banner__content > * {
+  display: inline-block;
+}
+/* 横にスクロールさせるアニメーション */
+@keyframes animate-banner {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
 }
 </style>
