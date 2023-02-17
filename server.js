38f17412b1651a04489f2fdf2d957db61ee0edf3
Ikuri Kijima <ikuri.kijima@rakus-partners.co.jp>
2023-02-17 14:27:14 +0900

    DB作成

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 const PORT = 8000;
 
+const prisma = new PrismaClient();
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  const { name, adress, TEL, email } = req.body;
+  const posts = await prisma.members.create({
+    data: {
+      name: name,
+      adress: adress,
+      TEL: TEL,
+      email: email,
+    },
+  });
+  return res.json(posts);
+});
 
 app.listen(PORT, () => {
   console.log("〜〜サーバーが起動中です〜〜");
-})
+});
