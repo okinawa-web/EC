745f64e4ef913c24c3db21b8717c716b4b2b1684
Ikuri Kijima <ikuri.kijima@rakus-partners.co.jp>
2023-02-22 10:26:18 +0900

    axios

--- server.js
+++ server.js
@@ -1,24 +1,55 @@
 const { PrismaClient } = require("@prisma/client");
+const { resolveComponentType } = require("@vue/compiler-core");
 const express = require("express");
 const app = express();
 const PORT = 8000;
+const cors = require("cors");
 
+app.use(cors());
 const prisma = new PrismaClient();
 app.use(express.json());
 
+//Member登録（ POST ）
+// app.post("/", async (req, res) => {
+//   const { name, address, tel, registerDate, email } = req.body;
+//   const posts = await prisma.Member.create({
+//     data: {
+//       name: name,
+//       address: address,
+//       tel: tel,
+//       registerDate: registerDate,
+//       email: email,
+//     },
+//   });
+//   return res.json(posts);
+// });
+
+// Reserve登録（ POST ）
 app.post("/", async (req, res) => {
-  const { name, adress, TEL, email } = req.body;
-  const posts = await prisma.members.create({
+  const { memberId, reservePeople, date } = req.body;
+  const posts = await prisma.Reserve.create({
     data: {
-      name: name,
-      adress: adress,
-      TEL: TEL,
-      email: email,
+      memberId: memberId,
+      reservePeople: reservePeople,
+      date: date,
     },
   });
   return res.json(posts);
 });
 
+//Memberデータ取得
+app.get("/member", async (req, res) => {
+  const member = await prisma.member.findMany();
+  return res.json(member);
+});
+
+//Memberデータ書き込み
+
+
+//起動チェック localhost:8000
+app.get("/", (req, res) => res.send("HELLO WORLD"));
+
+//起動チェック ターミナル
 app.listen(PORT, () => {
   console.log("〜〜サーバーが起動中です〜〜");
 });
