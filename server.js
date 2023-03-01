4cf22445c6350470adcc38cd5117699cc04eb5c6
Ikuri Kijima <ikuri.kijima@rakus-partners.co.jp>
2023-03-01 16:12:29 +0900

    16:12

--- server.js
+++ server.js
@@ -1,7 +1,52 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const { reactive } = require("vue");
+const prisma = new PrismaClient();
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
+
+app.use(express.json());
+
+app.get("/member", async (req, res) => {
+  const posts = await prisma.member.findMany();
+  res.json(posts);
+});
+
+app.get("/", async (req, res) => {
+  // const posts = await prisma.member.findMany();
+  res.send("こんにちは");
+});
+
+// app.get("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const post = await prisma.member.findUnique({
+//     where: {
+//       id: Number(id),
+//     }
+//   });
+//   res.json(post);
+// });
+
 
 app.listen(PORT, () => {
-  console.log("☆☆サーバー起動中☆☆");
+  console.log(`Example app listening at http://localhost:${PORT}`)
 });
+
+// app.post("/", async (req, res) => {
+//   const { id, name, address, tel, registerDate, email } = req.body;
+//   const posts = await prisma.member.create({
+//     data: {
+//       id: id,
+//       name: name,
+//       address: address,
+//       tel: tel,
+//       registerDate: registerDate,
+//       email: email,
+//     },
+//   });
+//   return res.json(posts);
+// });
+
+// app.listen(PORT, () => {
+//   console.log("☆☆サーバー起動中☆☆");
+// });
