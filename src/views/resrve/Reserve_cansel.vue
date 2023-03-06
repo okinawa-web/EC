e1f1731a6f95042ef86130d6ef141131a2af6cb4
Ikuri Kijima <ikuri.kijima@rakus-partners.co.jp>
2023-03-06 14:30:31 +0900

    予約キャンセル画面OK

--- src/views/resrve/Reserve_cansel.vue
+++ src/views/resrve/Reserve_cansel.vue
@@ -1,3 +1,124 @@
+<script setup>
+import ReserveHeader from "/Users/ikuri/Desktop/EC/src/components/reserve/ReaserveHeader.vue";
+</script>
 <template>
-  <h1>予約のキャンセル</h1>
+  
+  <ReserveHeader />
+  <div class="login_title">予約内容キャンセル｜HAMAJIMA LAND</div>
+  <div class="login_wrapper">
+    <div class="login_package">
+      <div class="login_box">
+        <h2>予約内容をキャンセルされますか？</h2>
+        <p>
+          予約No. と E-mailアドレス
+          を入力し「キャンセル手続き」ボタンをクリックして下さい。
+          E-mailアドレスはアルファベットの大文字と小文字が区別されます。お間違えのないようご注意ください。
+        </p>
+        <div class="login_form">
+          <div class="login_form form">
+            <table class="login_table">
+              <tr>
+                <th>予約No.</th>
+                <td>
+                  <input type="text" class="login_input" />
+                </td>
+              </tr>
+              <tr>
+                <th>予約時のE-mailアドレス</th>
+                <td><input type="text" class="login_input" /></td>
+              </tr>
+            </table>
+            <p class="forgot_password">
+              <a href="">パスワードを忘れた場合</a>
+            </p>
+          </div>
+          <ul class="ul_button">
+            <li class="li_button">
+              <input type="button" value="前の画面に戻る" class="back_button" />
+            </li>
+            <li class="li_button">
+              <input
+                type="button"
+                value="キャンセル手続きへ"
+                class="login_button"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+.login_wrapper {
+  width: 900px;
+  font-size: 100%;
+  margin: 0 auto;
+  border: 1px solid #a0a0a0;
+  padding: 0 20px;
+}
+
+.login_title {
+  background-color: rgb(0, 83, 92);
+  color: white;
+  font-size: 115%;
+  padding: 10px 18px 8px;
+  width: 900px;
+  margin: auto;
+  border: 3px solid rgb(0, 83, 92);
+}
+
+.LOGIN {
+  font-size: 143%;
+  font-weight: bold;
+  border-top: 3px solid #dcdcdc;
+  border-bottom: 3px solid #dcdcdc;
+  padding: 8px 15px;
+  margin-top: 30px;
+}
+
+.logon_form {
+  width: 70%;
+}
+
+.form {
+  margin-top: 40px;
+}
+.login_table {
+  margin: auto;
+}
+
+.login_input {
+  width: 50px;
+}
+.forgot_password {
+  text-align: center;
+}
+
+.li_button {
+  display: inline;
+}
+
+.ul_button {
+  width: 90%;
+  text-align: center;
+}
+
+.back_button,
+.login_button {
+  border: 1px solid #a0a0a0;
+  background-color: white;
+  padding: 15px 35px;
+  line-height: 1.2;
+  border-radius: 3px;
+  box-shadow: 0px 2px 2px #a0a0a0;
+  cursor: pointer;
+}
+
+.login_button {
+  background-color: rgb(0, 114, 114);
+  color: white;
+  margin-left: 15px;
+}
+</style>
