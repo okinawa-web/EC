3df66759609371fc1af514da13bd16581281cb5d
Ikuri Kijima <ikuri.kijima@rakus-partners.co.jp>
2023-03-31 15:48:58 +0900

    会員情報をお願いします

--- src/views/resrve/MyPage.vue
+++ src/views/resrve/MyPage.vue
@@ -1,7 +1,95 @@
 <script setup>
+import ReserveHeader from "@/components/reserve/ReaserveHeader.vue";
 
 </script>
 
 <template>
-  <h1>マイページ</h1>
+  <ReserveHeader />
+
+  <div class="login_title">会員情報｜HAMAJIMA LAND</div>
+  <div class="login_wrapper">
+    <div class="login_package">
+      <div class="login_box">
+        <h2>ご登録情報</h2>
+        <p>名前：</p>
+        <p>メールアドレス：</p>
+        <p>住所：</p>
+        <p>電話番号：</p>
+        
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+.login_wrapper {
+  width: 900px;
+  font-size: 100%;
+  margin: 0 auto;
+  border: 1px solid #a0a0a0;
+  padding: 0 20px;
+}
+
+.login_title {
+  background-color: rgb(0, 83, 92);
+  color: white;
+  font-size: 115%;
+  padding: 10px 18px 8px;
+  width: 900px;
+  margin: auto;
+  border: 3px solid rgb(0, 83, 92);
+}
+
+.LOGIN {
+  font-size: 143%;
+  font-weight: bold;
+  border-top: 3px solid #dcdcdc;
+  border-bottom: 3px solid #dcdcdc;
+  padding: 8px 15px;
+  margin-top: 30px;
+}
+
+.logon_form {
+  width: 70%;
+}
+
+.form {
+  margin-top: 40px;
+}
+.login_table {
+  margin: auto;
+}
+
+.login_input {
+  width: 50px;
+}
+.forgot_password {
+  text-align: center;
+}
+
+.li_button {
+  display: inline;
+}
+
+.ul_button {
+  width: 90%;
+  text-align: center;
+}
+
+.back_button,
+.login_button {
+  border: 1px solid #a0a0a0;
+  background-color: white;
+  padding: 15px 35px;
+  line-height: 1.2;
+  border-radius: 3px;
+  box-shadow: 0px 2px 2px #a0a0a0;
+  cursor: pointer;
+}
+
+.login_button {
+  background-color: rgb(0, 114, 114);
+  color: white;
+  margin-left: 15px;
+}
+</style>
